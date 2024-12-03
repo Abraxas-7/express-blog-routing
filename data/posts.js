e52aa3c772e987df752d87eb2b3e1eadb203e56e
Abraxas-7 <arber.beshaj.02@gmail.com>
2024-12-03 13:29:07 +0100

    creazione file

---
+++ data/posts.js
@@ -0,0 +1,42 @@
+const posts = {
+  "totalCount": 5,
+  "data": [
+    {
+      "id": 1,
+      "titolo": "Ciambellone classico",
+      "contenuto": "Un dolce soffice e profumato, perfetto per la colazione o una merenda leggera. Facile da preparare con ingredienti semplici come farina, zucchero, uova e burro.",
+      "immagine": "./img/ciambellone.jpeg",
+      "tags": ["dolci", "colazione", "merenda", "tradizione"]
+    },
+    {
+      "id": 2,
+      "titolo": "Cracker alla barbabietola",
+      "contenuto": "Snack croccanti e salutari preparati con farina integrale e barbabietola, ideali per aperitivi o spuntini leggeri.",
+      "immagine": "./img/cracker_barbabietola.jpeg",
+      "tags": ["snack", "vegetariani", "aperitivo", "salutare"]
+    },
+    {
+      "id": 3,
+      "titolo": "Pane fritto dolce",
+      "contenuto": "Un dessert sfizioso e croccante: fette di pane fritte e ricoperte di zucchero e cannella, perfette da gustare calde.",
+      "immagine": "./img/pane_fritto_dolce.jpeg",
+      "tags": ["dolci", "dessert", "facile", "veloce"]
+    },
+    {
+      "id": 4,
+      "titolo": "Pasta alla barbabietola",
+      "contenuto": "Un primo piatto colorato e saporito: pasta con crema di barbabietola e formaggio cremoso, perfetto per stupire gli ospiti.",
+      "immagine": "./img/pasta_barbabietola.jpeg",
+      "tags": ["primi piatti", "vegetariani", "colorato", "cremoso"]
+    },
+    {
+      "id": 5,
+      "titolo": "Torta paesana",
+      "contenuto": "Un dolce tradizionale a base di pane raffermo, latte, cacao e frutta secca. Una ricetta della tradizione lombarda per recuperare gli avanzi in modo goloso.",
+      "immagine": "./img/torta_paesana.jpeg",
+      "tags": ["dolci", "tradizione", "recupero", "frutta secca"]
+    }
+  ]
+};
+
+module.exports = posts;
